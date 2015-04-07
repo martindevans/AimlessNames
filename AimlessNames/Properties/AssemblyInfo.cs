8d74e16b52bae15da79cd1ec538afad267ff4459
Martin Evans <martindevans@gmail.com>
2015-04-07 20:18:30 +0100

    Added description Built and pushed to nuget (https://www.nuget.org/packages/AimlessNames/)
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- AimlessNames/Properties/AssemblyInfo.cs
+++ AimlessNames/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AimlessNames")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Generates gendered names in specific locales")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AimlessNames")]
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
